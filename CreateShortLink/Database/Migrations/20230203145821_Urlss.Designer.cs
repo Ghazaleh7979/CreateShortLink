de7570466c0957ff5762dd24bc5ef8f8134f9b2e
ghazaleh7979 <masoome.aliyannejhadi@gmail.com>
2023-02-03 18:28:44 +0330

    urlss

--- CreateShortLink/Database/Migrations/20230203145821_Urlss.Designer.cs
+++ CreateShortLink/Database/Migrations/20230203145821_Urlss.Designer.cs
@@ -2,6 +2,7 @@
 using CreateShortLink.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CreateShortLink.Database.Migrations
 {
     [DbContext(typeof(ModelDbContext))]
-    partial class ModelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230203145821_Urlss")]
+    partial class Urlss
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
